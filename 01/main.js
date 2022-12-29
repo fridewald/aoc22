1c1ff5cfcc5ca3d598080a81b3477c5a4ecbc16b
Julian Lukwata <julianlukwata@posteo.de>
2022-12-29 01:53:55 +0100

    exclude compile files

--- 01/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var fs = require("fs");
-var input = "./1/input.txt";
-var file = fs.readFileSync(input);
-console.log("hi");
-console.log(fie);
-//# sourceMappingURL=main.js.map
